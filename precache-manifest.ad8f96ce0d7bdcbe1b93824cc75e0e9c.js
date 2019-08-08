388860cabd4a026eb86bf95d9091fe1723e3dc9d
bojan30 <bojancuk33@gmail.com>
2019-08-08 21:41:56 +0200

    Updates

---
+++ precache-manifest.ad8f96ce0d7bdcbe1b93824cc75e0e9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522275113fc02da91bbfeb9be03faec7",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "20a2a29d65ed1566361a",
+    "url": "/react-todo-app/static/css/main.e8ac877a.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/react-todo-app/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "20a2a29d65ed1566361a",
+    "url": "/react-todo-app/static/js/main.8312024f.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
